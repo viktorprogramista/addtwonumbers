bc633c841840ff962aeaf11c9e6f27919b736720
Aakash20022001 <70940981+Aakash20022001@users.noreply.github.com>
2020-10-01 09:19:53 +0530

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
-int sum(int a, int b){return a+b;}
-int main() {
+int sum(int a, int b)
+{return a+b;}
+int main()
+{
   int a, b;
+  cout<<"Enter Two numbers:";
   cin >> a >> b;
-  cout << sum(a, b) << endl;
+  cout <<"Sum of Two NUmbers is:"<< sum(a, b) << endl;
 }
